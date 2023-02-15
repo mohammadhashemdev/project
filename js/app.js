72a0f9e93b5f58cfa853fc4d6b310e4e5232d698
Mohammad Hashem <hashimhanify@gmail.com>
2023-02-15 20:33:35 +0430

    burger menu

--- js/app.js
+++ js/app.js
@@ -3,11 +3,10 @@ const overlay = document.querySelector("#overlay");
 
 navToggler.addEventListener("click", function (e) {
   document.querySelector("#mobileNavbar").classList.toggle("translate-x-full");
-  overlay.classList.toggle("hidden");
-  overlay.style.opacity = 0.5;
+  overlay.classList.toggle("main-hidden");
 });
 
 overlay.addEventListener("click", function (e) {
   document.querySelector("#mobileNavbar").classList.toggle("translate-x-full");
-  overlay.classList.toggle("hidden");
+  overlay.classList.toggle("main-hidden");
 });
