21d6242dd53b5a0d7e2cb45116fcda88188b5af8
Mohammad Hashem <hashimhanify@gmail.com>
2023-02-17 18:38:29 +0430

    mobile navbar functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 const navToggler = document.querySelector(".togglerr");
 const overlay = document.querySelector("#overlay");
-const departments = document.querySelector("#departments");
+const mobileNavbar = document.querySelector("#mobileNavbar");
+const mobileNavItems = document.querySelectorAll("#mobileNavbar ul li");
+
+const goToSection = function () {
+  overlay.classList.add("main-hidden");
+  mobileNavbar.classList.add("translate-x-full");
+};
+
+mobileNavItems.forEach((item) => {
+  item.addEventListener("click", goToSection);
+});
 
 navToggler.addEventListener("click", function (e) {
-  document.querySelector("#mobileNavbar").classList.toggle("translate-x-full");
+  mobileNavbar.classList.toggle("translate-x-full");
   overlay.classList.toggle("main-hidden");
 });
 
