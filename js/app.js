0cd42e516dce14c1e1708f2bc533c4aec7dccfc0
Mohammad Hashem <hashimhanify@gmail.com>
2023-02-17 18:22:37 +0430

    footer

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const navToggler = document.querySelector(".togglerr");
 const overlay = document.querySelector("#overlay");
+const departments = document.querySelector("#departments");
 
 navToggler.addEventListener("click", function (e) {
   document.querySelector("#mobileNavbar").classList.toggle("translate-x-full");
